ab9e6286216ebcc837011a11461500721b1ef592
DamianoCasolari <damianocasolari@gmail.com>
2023-05-24 00:09:39 +0200

    add comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,17 @@
         <div class="tag">current series</div>
 
         <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6 py-4">
+            @foreach (config('db') as $item)
+                <a href="#" class="comic col py-4 px-3">
+                    <div class="img_container">
+                        <img src="{{ $item['thumb'] }}" alt="{{ $item['title'] }}">
+                    </div>
+                    <div class="text my-2 ms-1 fs-4"> {{ $item['title'] }}</div>
+                </a>
+            @endforeach
+
+
+
 
         </div>
     </div>
